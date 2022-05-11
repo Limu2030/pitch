a23040b689771d79377abc3ec96ac0ff6a261afa
limu2030 <limu2030@gmail.com>
2022-05-11 04:40:12 +0300

    configure mange.py file

--- manage.py
+++ manage.py
@@ -1,8 +1,7 @@
-from flask_bootstrap import Bootstrap
+# from flask_bootstrap import Bootstrap
 from flask import Flask
 from config import config_options
-
-bootstrap = Bootstrap()
+ 
 
 def create_app(config_name):
     app = Flask(__name__)
