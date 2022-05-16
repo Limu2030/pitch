b8ffe2a711537f09cb2cd5310a0f4c5dfec4d528
limu2030 <limu2030@gmail.com>
2022-05-16 16:59:01 +0300

    deploy to heroku

--- app/main/models.py
+++ app/main/models.py
@@ -7,6 +7,7 @@ from app import login_manager
 
 
 class User(UserMixin,db.Model):
+    __tablename__= 'users'
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(20),unique=True,nullable=False)
     email = db.Column(db.String(120),unique=True,nullable=False)
@@ -42,6 +43,7 @@ class Category(db.Model):
         return f"Category {self.name} " 
 
 class Pitch(db.Model):
+    __tablename__= 'pitches'
     id = db.Column(db.Integer,primary_key=True)
     title = db.Column(db.String(100),nullable=False)
     date_posted = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
