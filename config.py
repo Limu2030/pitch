cf09f97a3cdd49af18b0800b792b96fd21ca3dfa
limu2030 <limu2030@gmail.com>
2022-05-16 16:44:46 +0300

    deploy to heroku

--- config.py
+++ config.py
@@ -3,11 +3,9 @@ import os
 
 
 class Config:
-    DEBUG = os.environ.get('DEBUG')
     SECRET_KEY= os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
 
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://fmeutmnsdzdqww:567b5f96281c8fca3b55dd1e364e7df6953150b2eaccbfa3ea9157e4fe9bb793@ec2-54-86-224-85.compute-1.amazonaws.com:5432/d602ucgp8u0q7h'
