0752b44e2c6db3c5f8272b447d773cc01d359152
limu2030 <limu2030@gmail.com>
2022-05-17 15:13:11 +0300

    deployment

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
 class ProdConfig(Config):
     # SQLALCHEMY_DATABASE_URI = 'postgresql://ynzxcynqwkzwgu:d24546c110c7ca60e310f6bc1ae1d4cba7ea2b151cdb96dce4a6aa5b0cdd70f9@ec2-44-194-117-205.compute-1.amazonaws.com:5432/d45d0c3aat5480'
                             
-    SQLALCHEMY_DATABASE_URI='postgres://wgpxvhudzrcosd:1e93513e7238b0dc344a40fef2ca70073c27b0e480749df88511ad9096a57a6c@ec2-54-86-224-85.compute-1.amazonaws.com:5432/d93lur41ej8f2s'
+    SQLALCHEMY_DATABASE_URI='postgresql://wgpxvhudzrcosd:1e93513e7238b0dc344a40fef2ca70073c27b0e480749df88511ad9096a57a6c@ec2-54-86-224-85.compute-1.amazonaws.com:5432/d93lur41ej8f2s'
 
 class DevConfig(Config):
     DEBUG = True
